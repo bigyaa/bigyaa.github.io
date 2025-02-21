c1974424c1de128084b3ad5345e9ac4f70de6a5e
Bigya <bigyabajracharya@gmail.com>
2025-02-20 23:39:24 -0500

    Fix nextjs 404 config issue

--- next.config.js
+++ next.config.js
@@ -1,23 +1,14 @@
+const isProd = process.env.NODE_ENV === "production";
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // Correctly set the basePath for GitHub Pages
-  basePath: process.env.GITHUB_ACTIONS ? '/bigyaa.github.io' : '',
-
-  output: 'export', // ✅ Required for static exports (GitHub Pages)
-  trailingSlash: true, // ✅ Ensures URLs work correctly
-
-  images: {
-    unoptimized: true, // ✅ Required because GitHub Pages doesn’t support Next.js image optimization
-  },
-
-  webpack: (config, { isServer }) => {
-    if (!isServer) {
-      config.optimization.splitChunks = {
-        chunks: 'all',
-        minSize: 30000, // Default is 30 KB (good for performance)
-        maxSize: 250000, // Avoids too many small chunks
-      };
-    }
+  output: "export",
+  basePath: isProd ? "/bigyaa.github.io" : "",
+  assetPrefix: isProd ? "/bigyaa.github.io/" : "",
+  trailingSlash: true,
+  images: { unoptimized: true },
+  webpack: (config) => {
+    config.output.publicPath = isProd ? "/bigyaa.github.io/.next/" : "/.next/";
     return config;
   },
 };
