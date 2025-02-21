9c8330b08503e7f6df7b6e905b3250e8c325eaf2
Bigya <bigyabajracharya@gmail.com>
2025-02-21 15:01:17 -0500

    Remove base path to prevent duplicate nameswhen routing

--- next.config.js
+++ next.config.js
@@ -1,9 +1,6 @@
-const isProd = process.env.NODE_ENV === "production";
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
-  assetPrefix: isProd ? "/bigyaa.github.io/" : "", // ✅ Fixes GitHub Pages paths
   trailingSlash: true,
   images: { unoptimized: true },
 };
